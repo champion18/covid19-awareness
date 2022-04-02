f7daa6133e2f90a17ab6f262683eb6214efca600
champion18 <76389692+champion18@users.noreply.github.com>
2022-04-02 10:20:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-//no use const countryName = document.querySelector("#country");
+// fix size of window
+// add gap at top
+
 const form = document.querySelector("#searchForm");
+
+const initialData=async()=>{
+  const res = await axios.get("https://api.covid19api.com/summary");
+  const globalData=res.data.Global;
+  console.log(res.data);
+
+  const initialUpdated = document.getElementById("lastUpdated");
+  lastUpdated.textContent=globalData.Date.substring(0, 10);;
+
+  const totalConfirmed = document.getElementById("totalConfirmed");
+  totalConfirmed.textContent = globalData.TotalConfirmed;
+
+  const newConfirmed = document.getElementById("newConfirmed");
+  newConfirmed.textContent = globalData.NewConfirmed;
+
+  const totalDeaths = document.getElementById("totalDeaths");
+  totalDeaths.textContent = globalData.TotalDeaths;
+  const newDeaths = document.getElementById("newDeaths");
+  newDeaths.textContent = globalData.NewDeaths;
+}
+
+initialData();
 
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   //console.dir(form);
   const countryName = form.elements["country"].value;
+  form.elements["country"].value='';
   //console.log(countryName)
   const res = await axios.get("https://api.covid19api.com/summary");
   showData(res.data.Countries, countryName);
-  //console.log(res.data.Countries);
+  console.log(res.data.Countries);
 });
 
 const showData = (countries, countryName) => {
+  var flag=0;
   for (let country of countries) {
     const name = country.Country;
     if (name.toLowerCase() == countryName.toLowerCase()) {
@@ -19,10 +45,17 @@ const showData = (countries, countryName) => {
       console.log(country);
       //console.log(country.TotalConfirmed);
       setData(country);
+      flag=1;
     }
     //const foundCountry = countries.find((name) => name == countryName);
   }
+  if(flag==0)
+  handleInvalid();
 };
+
+const handleInvalid= () => {
+  const heading2 = document.getElementById("heading2");
+  alert('The country you entered is either invalid or not supported by this app. Please try again.');}
 
 const setData = (country) => {
   const lastUpdated = document.getElementById("lastUpdated");
