37944fb58530d685e08e7406ab9ae4e486507e96
champion18 <76389692+champion18@users.noreply.github.com>
2022-06-24 15:12:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 
 const form = document.querySelector("#searchForm");
 
-const initialData=async()=>{
+const initialData = async () => {
   const res = await axios.get("https://api.covid19api.com/summary");
-  const globalData=res.data.Global;
-  console.log(res.data);
+  const globalData = res.data.Global;
 
   const initialUpdated = document.getElementById("lastUpdated");
-  lastUpdated.textContent=globalData.Date.substring(0, 10);;
+  lastUpdated.textContent = globalData.Date.substring(0, 10);
 
   const totalConfirmed = document.getElementById("totalConfirmed");
   totalConfirmed.textContent = globalData.TotalConfirmed;
@@ -19,9 +18,10 @@ const initialData=async()=>{
 
   const totalDeaths = document.getElementById("totalDeaths");
   totalDeaths.textContent = globalData.TotalDeaths;
+
   const newDeaths = document.getElementById("newDeaths");
   newDeaths.textContent = globalData.NewDeaths;
-}
+};
 
 initialData();
 
@@ -29,7 +29,7 @@ form.addEventListener("submit", async function (e) {
   e.preventDefault();
   //console.dir(form);
   const countryName = form.elements["country"].value;
-  form.elements["country"].value='';
+  form.elements["country"].value = "";
   //console.log(countryName)
   const res = await axios.get("https://api.covid19api.com/summary");
   showData(res.data.Countries, countryName);
@@ -37,7 +37,7 @@ form.addEventListener("submit", async function (e) {
 });
 
 const showData = (countries, countryName) => {
-  var flag=0;
+  var flag = 0;
   for (let country of countries) {
     const name = country.Country;
     if (name.toLowerCase() == countryName.toLowerCase()) {
@@ -45,17 +45,19 @@ const showData = (countries, countryName) => {
       console.log(country);
       //console.log(country.TotalConfirmed);
       setData(country);
-      flag=1;
+      flag = 1;
     }
     //const foundCountry = countries.find((name) => name == countryName);
   }
-  if(flag==0)
-  handleInvalid();
+  if (flag == 0) handleInvalid();
 };
 
-const handleInvalid= () => {
+const handleInvalid = () => {
   const heading2 = document.getElementById("heading2");
-  alert('The country you entered is either invalid or not supported by this app. Please try again.');}
+  alert(
+    "The country you entered is either invalid or not supported by this app. Please try again."
+  );
+};
 
 const setData = (country) => {
   const lastUpdated = document.getElementById("lastUpdated");
---
+++ news.js
@@ -0,0 +1,43 @@
+// const axios = require("axios");
+const apiKey = "5c0abc89ec8745fe8b6ab48ec1bbccf8";
+
+const displayNews = (articles) => {
+  const news = document.querySelector(".news");
+
+  for (let i = 0; i < 3; i++) {
+    const newsCard = document.createElement("div");
+    newsCard.className = "newsCard";
+    news.appendChild(newsCard);
+
+    const img = document.createElement("img");
+    img.src = articles[i].media;
+    img.alt = articles[i].title;
+    newsCard.appendChild(img);
+
+    const heading = document.createElement("p");
+    heading.innerHTML = articles[i].title;
+    heading.className = "text";
+    newsCard.appendChild(heading);
+
+    const source = document.createElement("a");
+    source.innerHTML = articles[i].rights;
+    source.href = articles[i].link;
+    heading.appendChild(source);
+  }
+};
+
+const getNews = async () => {
+  const res = await axios.get(
+    "https://free-news.p.rapidapi.com/v1/search?q=covid&lang=en",
+    {
+      headers: {
+        "X-RapidAPI-Key": "9ee3845df0msh500703b88f843e2p1bcb10jsn1c0a538234de",
+        "X-RapidAPI-Host": "free-news.p.rapidapi.com",
+      },
+    }
+  );
+  console.log(res.data.articles);
+  displayNews(res.data.articles);
+};
+
+getNews();
